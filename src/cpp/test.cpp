6ea8477302f34740a2bc924ba0595f4065394c63
D14na Support <support@d14na.org>
2019-08-05 08:22:51 -0400

    Added cpp test.

---
+++ src/cpp/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello " << std::endl;
+
+    std::string name;
+
+    std::cout << "Enter your name" << std::endl;
+
+    std::cin >> name;
+
+    std::cout << "OK " << name << ", welcome to da' par-tay!!" << std::endl;
+
+    return 0;
+}
